4ef766745edede656838ebdac23fe1c530cf445b
shuo <1044163771@qq.com>
2020-05-18 22:00:46 +0800

    创建FCFS

--- dispatch/job.c
+++ dispatch/job.c
@@ -1,4 +1,4 @@
-#include "jobtime.h"
+#include "job.h"
 std::ostream & operator<<(std::ostream &output, Job &job) {
     output << "JobID:" << job.jobId
         << "\tArrivalTime:" << job.arrivalTime
@@ -6,17 +6,15 @@ std::ostream & operator<<(std::ostream &output, Job &job) {
         << "\tPriority:" << job.priority;
     return output;
 }
-//按到达时间排序函数
+
 int jobSortArrivalTime(Job& left, Job& right) {
     int x = left.arrivalTime - right.arrivalTime;
     return x < 0 ? -1 : x > 0 ? 1 : 0;
 }
-//按执行时间长短排序函数
 int jobSortExecutionTime(Job& left, Job& right) {
     int x = left.executionTime - right.executionTime;
     return x < 0 ? -1 : x > 0 ? 1 : 0;
 }
-//按优先级排序函数
 int jobSortPriority(Job& left, Job& right) {
     int x = left.priority - right.priority;
     return x < 0 ? -1 : x > 0 ? 1 : 0;
--- dispatch/job.h
+++ dispatch/job.h
@@ -11,15 +11,11 @@ struct Job {
     unsigned int priority;
 };
 
-//输出流重载
 std::ostream& operator<<(std::ostream &, Job&);
 
-//按到达时间排序函数
 int jobSortArrivalTime(Job&, Job&);
 
-//按执行时间长短排序函数
 int jobSortExecutionTime(Job&, Job&);
 
-//按优先级排序函数
 int jobSortPriority(Job&, Job&);
 
